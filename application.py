cb4e84bb05db5bb4ad43924424f71a64fd74b67b
Avinash-Gangwal <avinash.gangwal@gmail.com>
2024-10-06 10:15:28 -0700

    Adding sample app

---
+++ application.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+
+app_path = os.path.join(os.path.dirname(__file__), '.')
+dotenv_path = os.path.join(app_path, '.env')
+load_dotenv(dotenv_path)
+
+app = Flask("TarotReader")
+app.config.update(
+    
+    DEBUG= bool(os.environ.get("DEBUG", False)),
+    PORT=os.environ.get("PORT"),
+    DEPLOYMENT=os.environ.get("DEPLOYMENT")
+
+)
+@app.route("/")
+def index():
+    return "Welcome to TarotReader here again" 
+@app.route('/about')
+def about():
+    return "This is about page"  
+app.run(debug=app.config["DEBUG"], host='0.0.0.0',port=int (app.config["PORT"]))
